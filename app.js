f5ba7ae89ea31d0e09d9cffed82165a61d4c38da
Fernanda Murched <fernanda_hco@outlook.com.br>
2025-08-17 13:50:01 -0300

    :sparkles: Construção do Challenge

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+  let input = document.getElementById("amigo");
+  let nome = input.value.trim();
+
+  if(nome == '') {
+    alert("Por favor, digite um nome válido");
+    return;
+  }
+
+  amigos.push(nome);
+  input.value = '';
+  
+  atualizarLista();
+}
+
+function atualizarLista() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = ""; // limpa a lista antes de atualizar
+
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li"); // cria um <li>
+    item.textContent = amigos[i]; // coloca o nome dentro
+    lista.appendChild(item); // adiciona <li> na <ul>
+  }
+}
+
+function sortearAmigo(){
+  if (amigos.length === 0) {
+    alert("A lista de amigos está vazia!");
+    return;
+  }
+
+  let indiceSorteado = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceSorteado];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "🎉 Amigo sorteado é: " + amigoSorteado + "🎉";
+}
